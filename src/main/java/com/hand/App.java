97e1b2f6c068085f9ee5af43406b6e9db9c855ce
wayne <842269153@qq.com>
2017-03-16 16:06:54 +0800

    add gitignore

--- src/main/java/com/hand/App.java
+++ src/main/java/com/hand/App.java
@@ -1,5 +1,7 @@
 package com.hand;
 
+import java.util.Random;
+
 /**
  * Hello world!
  *
@@ -9,5 +11,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        Random r = new Random();
+        System.out.println(r.nextBoolean());
     }
 }
